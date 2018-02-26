2fd771731560d2d53753db41822d24fd03939332
Michael Jewell <michael_jewell@hotmail.co.uk>
2018-02-27 11:56:26 +1300

    Added GoogleRecaptchaHelper

---
+++ src/Arragro.Core.Web/Helpers/GoogleRecaptchaHelper.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Arragro.Core.Web.Helpers
+{
+    public class GoogleReCaptcha
+    {
+        public static bool Validate(string encodedResponse, string googleSecret)
+        {
+            var client = new System.Net.WebClient();
+
+            var googleReply = client.DownloadString($@"https://www.google.com/recaptcha/api/siteverify?secret={googleSecret}&response={encodedResponse}");
+
+            var captchaResponse = JsonConvert.DeserializeObject<ReCaptcha>(googleReply);
+
+            return captchaResponse.Success;
+        }
+
+        private bool _success;
+        [JsonProperty("success")]
+        internal bool Success
+        {
+            get { return _success; }
+            set { _success = value; }
+        }
+
+        private List<string> _errorCodes;
+        [JsonProperty("error-codes")]
+        internal List<string> ErrorCodes
+        {
+            get { return _errorCodes; }
+            set { _errorCodes = value; }
+        }
+    }
+}
