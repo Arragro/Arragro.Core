ce284523b6db9f3101fd24c594b3c83b9b6bcbf2
Michael Jewell <michael_jewell@hotmail.co.uk>
2018-06-28 15:19:20 +1200

    Introduced ConfigureDataProtection and associated models

--- src/Arragro.Core.Common/Models/BaseSettings.cs
+++ src/Arragro.Core.Common/Models/BaseSettings.cs
@@ -1,17 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Arragro.Core.Common.Enums;
+using System;
 
 namespace Arragro.Core.Common.Models
 {
     public class WebInfoSettings
     {
-        public bool IsWebInfoEnabled { get; set; }
+        public bool IsWebInfoEnabled { get; set; } = false;
         public Guid Secret { get; set; } = Guid.Empty;
+    }
+
+    /// <summary>
+    /// DataProtectionSettings is used to configure DataProtection.  Useful for multiple instances
+    /// </summary>
+    public class DataProtectionSettings
+    {
+        /// <summary>
+        /// Determines if X509 cert is to be used, required for multi instance apps, replaces machine key
+        /// </summary>
+        public bool UseX509 { get; set; } = false;
+        /// <summary>
+        /// Base64 encoded byte array of the cert (cert should be pfx with password)
+        /// </summary>
+        public string CertBase64 { get; set; } = null;
+        /// <summary>
+        /// Password to the CertBase64 cert
+        /// </summary>
+        public string Password { get; set; } = null;
+        /// <summary>
+        /// Storage provider
+        /// </summary>
+        public DataProtectionStorage DataProtectionStorage { get; set; } = DataProtectionStorage.FileSystem;
+        /// <summary>
+        /// Connection string to redis
+        /// </summary>
+        public string RedisConnection { get; set; } = null;
+        /// <summary>
+        /// Path to the file storeage is FileSystem is set on DataProtectionStorage
+        /// </summary>
+        public string DataProtectionStoragePath { get; set; } = null;
     }
 
     public class BaseSettings
     {
-        public WebInfoSettings WebInfoSettings { get; set; }
+        public WebInfoSettings WebInfoSettings { get; set; } = new WebInfoSettings();
+        public DataProtectionSettings DataProtectionSettings { get; set; } = new DataProtectionSettings();
     }
 }
---
+++ src/Arragro.Core.Web/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Arragro.Core.Common.Enums;
+using Arragro.Core.Common.Models;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Arragro.Core.Web.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureDataProtection(
+            this IServiceCollection services,
+            BaseSettings baseSettings)
+        {
+            var dataProtection = services.AddDataProtection();
+
+            if (baseSettings.DataProtectionSettings.UseX509 &&
+                !string.IsNullOrWhiteSpace(baseSettings.DataProtectionSettings.CertBase64) &&
+                !string.IsNullOrWhiteSpace(baseSettings.DataProtectionSettings.Password))
+            {
+                /*
+                 * https://docs.microsoft.com/en-us/aspnet/core/security/data-protection/?view=aspnetcore-2.1
+                 * https://github.com/aspnet/DataProtection
+                 * https://docs.microsoft.com/en-us/aspnet/core/security/data-protection/compatibility/replacing-machinekey?view=aspnetcore-2.1
+                 * http://www.paraesthesia.com/archive/2016/06/15/set-up-asp-net-dataprotection-in-a-farm/
+                 * Create crt and key in ubuntu bash:
+                 * openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:4096 -keyout privateKey.key -out certificate.crt
+                 * 
+                 * Create PFX from crt and key
+                 * openssl pkcs12 -export -out certificate.pfx -inkey privateKey.key -in certificate.crt
+                 * 
+                 * Add the cert to the certmgr local machine - you can get it at: C:\Users\{UserName}\AppData\Local\lxss\root
+                 * 
+                 */
+
+                var bytes = Convert.FromBase64String(baseSettings.DataProtectionSettings.CertBase64);
+                X509Certificate2 x509Cert = new X509Certificate2(bytes, baseSettings.DataProtectionSettings.Password);
+
+                dataProtection.ProtectKeysWithCertificate(x509Cert);
+            }
+
+            if (baseSettings.DataProtectionSettings.DataProtectionStorage == DataProtectionStorage.Redis &&
+                !string.IsNullOrWhiteSpace(baseSettings.DataProtectionSettings.RedisConnection))
+            {
+                var redis = ConnectionMultiplexer.Connect(baseSettings.DataProtectionSettings.RedisConnection);
+
+                dataProtection.PersistKeysToRedis(redis, "DataProtection-Keys");
+            }
+
+            if (baseSettings.DataProtectionSettings.DataProtectionStorage == DataProtectionStorage.FileSystem &&
+                !string.IsNullOrEmpty(baseSettings.DataProtectionSettings.DataProtectionStoragePath))
+            {
+                dataProtection.PersistKeysToFileSystem(new DirectoryInfo(baseSettings.DataProtectionSettings.DataProtectionStoragePath));
+            }
+
+            return services;
+        }
+    }
+}
