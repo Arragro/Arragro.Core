b8b7bda12e46229ad28034831f8539c1d857ad5c
Michael Jewell <michael_jewell@hotmail.co.uk>
2018-10-04 10:48:47 +1300

    Added UserNameResolver to Arragro.Core.Identity

--- src/Arragro.Core.DistributedCache/DistributedCacheManager.cs
+++ src/Arragro.Core.DistributedCache/DistributedCacheManager.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Arragro.Core.DistributedCacheManager
+namespace Arragro.Core.DistributedCache
 {
     public class DistributedCacheManager
     {
---
+++ src/Arragro.Core.Identity/Resolvers/UserNameResolver.cs
@@ -0,0 +1,34 @@
+﻿
+using Arragro.Core.DistributedCache;
+using Arragro.Core.Identity.Domains;
+using Arragro.Core.Identity.Models;
+using AutoMapper;
+using System;
+
+namespace Arragro.Core.Identity.Resolvers
+{
+    public class UserNameResolver : IMemberValueResolver<object, object, Guid, string>
+    {
+        private readonly Users<User> _users;
+        private readonly DistributedCacheManager _distributedCacheManager;
+
+        public UserNameResolver(
+            Users<User> users,
+            DistributedCacheManager distributedCacheManager)
+        {
+            _users = users;
+            _distributedCacheManager = distributedCacheManager;
+        }
+
+        public string Resolve(object source, object destination, Guid sourceMember, string destMember, ResolutionContext context)
+        {
+            return _distributedCacheManager.Get($"UserNameResolver:{sourceMember}", () =>
+            {
+                var user = _users.GetAsync(sourceMember).Result;
+                if (user == null)
+                    return "Something is wrong with this users data";
+                return user.UserName;
+            });
+        }
+    }
+}
