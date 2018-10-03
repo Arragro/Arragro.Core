ed27696a134029205fcb633711534d7d700fdd00
Michael Jewell <michael_jewell@hotmail.co.uk>
2018-10-04 11:27:01 +1300

    ArragroCoreIdentityBaseContext no accepts DbOptions

--- src/Arragro.Core.Identity/ArragroCoreIdentityBaseContext.cs
+++ src/Arragro.Core.Identity/ArragroCoreIdentityBaseContext.cs
@@ -1,5 +1,6 @@
 ﻿using Arragro.Core.Identity.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace Arragro.Core.Identity
@@ -8,5 +9,6 @@ namespace Arragro.Core.Identity
         where TUser : User
         where TRole : Role
     {
+        public ArragroCoreIdentityBaseContext(DbContextOptions options) : base(options) { }
     }
 }
