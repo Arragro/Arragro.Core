f89c05c8172992eaedd4e81ed2af57437cc22248
Michael Jewell <michael_jewell@hotmail.co.uk>
2019-05-07 14:40:13 +1200

    Further snake case changes

--- src/Arragro.Core.Identity/ArragroCoreIdentityBaseContext.cs
+++ src/Arragro.Core.Identity/ArragroCoreIdentityBaseContext.cs
@@ -1,13 +1,35 @@
-﻿using Arragro.Core.Identity.Models;
+﻿using Arragro.Core.EntityFrameworkCore.Extensions;
+using Arragro.Core.Identity.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Text.RegularExpressions;
 
 namespace Arragro.Core.Identity
 {
+    public static class StringExtensions
+    {
+        public static string ToSnakeCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) { return input; }
+
+            var startUnderscores = Regex.Match(input, @"^_+");
+            return startUnderscores + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
+        }
+    }
+
     public class ArragroCoreIdentityBaseContext : IdentityDbContext<User, Role, Guid>
     {
         public ArragroCoreIdentityBaseContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultSchema("identity");
+
+            modelBuilder.SnakeCaseTablesAndProperties();
+        }
     }
 
     public class ArragroCoreIdentityContext : ArragroCoreIdentityBaseContext
