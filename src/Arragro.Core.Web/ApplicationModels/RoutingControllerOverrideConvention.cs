9f28f6e042b3a3bbcf8f143df015a4d294e44d22
Michael Jewell <michael_jewell@hotmail.co.uk>
2019-05-20 10:39:30 +1200

    Added RoutingControllerOverrideConvention
    This makes overriding a controller easier between projects

---
+++ src/Arragro.Core.Web/ApplicationModels/RoutingControllerOverrideConvention.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+using System.Collections.Generic;
+
+namespace Arragro.Core.Web.ApplicationModels
+{
+    public class RoutingControllerOverrideConvention : IApplicationModelConvention
+    {
+        private readonly List<Type> _removals;
+
+        public RoutingControllerOverrideConvention(List<Type> removals)
+        {
+            _removals = removals;
+        }
+
+        public void Apply(ApplicationModel application)
+        {
+            var removeList = new List<ControllerModel>();
+            foreach (var controller in application.Controllers)
+            {
+                if (_removals.Contains(controller.ControllerType))
+                {
+                    removeList.Add(controller);
+                }
+            }
+            foreach (var remove in removeList)
+                application.Controllers.Remove(remove);
+        }
+    }
+}
