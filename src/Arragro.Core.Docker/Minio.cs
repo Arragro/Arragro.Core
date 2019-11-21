7a1923fe329cabcc35e3ffef7b70c4e77d2b3919
Mike Jewell <mike@arragro.com>
2019-11-22 09:44:13 +1300

    Added Minio docker for testing

---
+++ src/Arragro.Core.Docker/Minio.cs
@@ -0,0 +1,64 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Arragro.Core.Docker
+{
+    public static class Minio
+    {
+        public static async Task<ContainerListResponse> StartMinio(DockerClient client)
+        {
+            const string ContainerName = "minio";
+            const string ImageName = "minio/minio";
+            const string ImageTag = "RELEASE.2019-10-12T01-39-57Z";
+
+            await DockerExtentions.EnsureImageExistsAndCleanupAsync(client, ImageName, ImageTag, ContainerName);
+
+            var config = new Config();
+
+            var hostConfig = new HostConfig
+            {
+                PublishAllPorts = true,
+                PortBindings = new Dictionary<string, IList<PortBinding>>
+                {
+                    { "9000/tcp", new List<PortBinding> { new PortBinding { HostIP = "", HostPort = "9000" } } }
+                }
+            };
+
+            await client.Containers.CreateContainerAsync(new CreateContainerParameters(config)
+            {
+                Image = ImageName + ":" + ImageTag,
+                Name = ContainerName,
+                ExposedPorts = new Dictionary<string, object>() {
+                    { "9000/tcp", new { HostPort = 9000.ToString() } }
+                } as IDictionary<string, EmptyStruct>,
+                HostConfig = hostConfig,
+                Env = new List<string>
+                {      
+                    "MINIO_REGION=us-east-1",
+                    "MINIO_ACCESS_KEY=minio_access_key",
+                    "MINIO_SECRET_KEY=minio_secret_key"
+                },
+                Cmd = new List<string>
+                {
+                    "server /data"
+                }
+            });
+
+            var container = await DockerExtentions.GetContainerAsync(client, ContainerName);
+            if (container == null)
+                throw new Exception("No minio container.");
+
+            if (container.State != "running")
+            {
+                var started = await client.Containers.StartContainerAsync(container.ID, new ContainerStartParameters());
+                if (!started)
+                    throw new Exception("Cannot start the minio docker container.");
+            }
+
+            return container;
+        }
+    }
+}
