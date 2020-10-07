9d3a544d1e9b5af7ddbca064ad2b05dd02a62ac8
Mike Jewell <mike@arragro.com>
2020-10-07 14:49:44 +1300

    Added a cookie service to the web project

---
+++ src/Arragro.Core.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arragro.Core.Web.Extensions
+{
+    public static class StringExtensions
+	{
+		public static string FromBase64String(this string value, bool throwException = true)
+		{
+			try
+			{
+				byte[] decodedBytes = Convert.FromBase64String(value);
+				string decoded = System.Text.Encoding.UTF8.GetString(decodedBytes);
+
+				return decoded;
+			}
+			catch (Exception ex)
+			{
+				if (throwException)
+					throw new Exception(ex.Message, ex);
+				else
+					return value;
+			}
+		}
+
+		public static string ToBase64String(this string value)
+		{
+			byte[] bytes = System.Text.Encoding.UTF8.GetBytes(value);
+			string encoded = Convert.ToBase64String(bytes);
+
+			return encoded;
+		}
+	}
+}
---
+++ src/Arragro.Core.Web/Middleware/CookieServiceMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Arragro.Core.Web.Extensions;
+using Arragro.Core.Web.Services;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Arragro.Core.Web.Middleware
+{
+    public class CookieServiceMiddleware : IMiddleware
+    {
+        private readonly ICookieService _cookieService;
+
+        public CookieServiceMiddleware(
+            ICookieService cookieService)
+        {
+            _cookieService = cookieService;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            // write cookies to response right before it starts writing out from MVC/api responses...
+            context.Response.OnStarting(() =>
+            {
+                // cookie service should not write out cookies on 500, possibly others as well
+                if (!context.Response.StatusCode.IsInRange(500, 599))
+                {
+                    _cookieService.WriteToResponse(context);
+                }
+                return Task.CompletedTask;
+            });
+
+            await next(context);
+        }
+    }
+}
