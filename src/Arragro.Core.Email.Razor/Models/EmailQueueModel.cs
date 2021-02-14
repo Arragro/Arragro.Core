56086afcc7ef20392d5adbf706e1f498725558e5
Mike Jewell <mike@arragro.com>
2021-02-14 13:25:07 +1300

    Removed EmailMessage from EmailQueueModel

--- src/Arragro.Core.Email.Razor/Models/EmailQueueModel.cs
+++ src/Arragro.Core.Email.Razor/Models/EmailQueueModel.cs
@@ -1,20 +1,12 @@
-﻿using Arragro.Core.Common.Models;
-using System;
+﻿using System;
 
 namespace Arragro.Core.Email.Razor.Models
 {
-    public class EmailQueueModel : EmailMessage
+    public class EmailQueueModel
     {
         public Guid ArragroId { get; set; }
 
-        public EmailQueueModel(string subject, EmailAddress from, string text, string html, EmailAddress to)
-            : base(subject, from, text, html, to)
-        {
-            ArragroId = Guid.NewGuid();
-        }
-
-        public EmailQueueModel(string subject, string text, string html, EmailAddress to)
-            : base(subject, text, html, to)
+        public EmailQueueModel()
         {
             ArragroId = Guid.NewGuid();
         }
