65a0b62dc1b4813a3dee2fc0d421a628e2d69285
Mike Jewell <mike@arragro.com>
2021-02-18 22:13:03 +1300

    Updated docker azurite
    Aplit for the new storage apis

--- src/Arragro.Core.Docker/Azurite.cs
+++ src/Arragro.Core.Docker/Azurite.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Arragro.Core.Docker
 {
-    public static class Azurite
+    public static class AzuriteTables
     {
-        public static async Task<ContainerListResponse> StartAzurite(DockerClient client)
+        public static async Task<ContainerListResponse> StartAzuriteTables(DockerClient client)
         {
             const string ContainerName = "azurite";
             const string ImageName = "touchify/azurite";
@@ -23,8 +23,6 @@ namespace Arragro.Core.Docker
                 PublishAllPorts = true,
                 PortBindings = new Dictionary<string, IList<PortBinding>>
                 {
-                    { "10000/tcp", new List<PortBinding> { new PortBinding { HostIP = "", HostPort = "10000" } } },
-                    { "10001/tcp", new List<PortBinding> { new PortBinding { HostIP = "", HostPort = "10001" } } },
                     { "10002/tcp", new List<PortBinding> { new PortBinding { HostIP = "", HostPort = "10002" } } }
                 }
             };
@@ -34,8 +32,6 @@ namespace Arragro.Core.Docker
                 Image = ImageName + ":" + ImageTag,
                 Name = ContainerName,
                 ExposedPorts = new Dictionary<string, object>() {
-                    { "10000/tcp", new { HostPort = 10000.ToString() } },
-                    { "10001/tcp", new { HostPort = 10001.ToString() } },
                     { "10002/tcp", new { HostPort = 10002.ToString() } }
                 } as IDictionary<string, EmptyStruct>,
                 HostConfig = hostConfig
@@ -43,13 +39,60 @@ namespace Arragro.Core.Docker
 
             var container = await DockerExtentions.GetContainerAsync(client, ContainerName);
             if (container == null)
-                throw new Exception("No azurite container.");
+                throw new Exception("No azurite tables container.");
 
             if (container.State != "running")
             {
                 var started = await client.Containers.StartContainerAsync(container.ID, new ContainerStartParameters());
                 if (!started)
-                    throw new Exception("Cannot start the azurite docker container.");
+                    throw new Exception("Cannot start the azurite tables docker container.");
+            }
+
+            return container;
+        }
+    }
+    public static class AzuriteMicrosoft
+    {
+        public static async Task<ContainerListResponse> StartAzuriteMicrosoft(DockerClient client)
+        {
+            const string ContainerName = "azurite";
+            const string ImageName = "mcr.microsoft.com/azure-storage/azurite";
+            const string ImageTag = "3.11.0";
+
+            await DockerExtentions.EnsureImageExistsAndCleanupAsync(client, ImageName, ImageTag, ContainerName);
+
+            var config = new Config();
+
+            var hostConfig = new HostConfig
+            {
+                PublishAllPorts = true,
+                PortBindings = new Dictionary<string, IList<PortBinding>>
+                {
+                    { "10000/tcp", new List<PortBinding> { new PortBinding { HostIP = "", HostPort = "10000" } } },
+                    { "10001/tcp", new List<PortBinding> { new PortBinding { HostIP = "", HostPort = "10001" } } }
+                }
+            };
+
+            await client.Containers.CreateContainerAsync(new CreateContainerParameters(config)
+            {
+                Image = ImageName + ":" + ImageTag,
+                Name = ContainerName,
+                ExposedPorts = new Dictionary<string, object>() {
+                    { "10000/tcp", new { HostPort = 10000.ToString() } },
+                    { "10001/tcp", new { HostPort = 10001.ToString() } }
+                } as IDictionary<string, EmptyStruct>,
+                HostConfig = hostConfig
+            });
+
+            var container = await DockerExtentions.GetContainerAsync(client, ContainerName);
+            if (container == null)
+                throw new Exception("No azurite microsoft container.");
+
+            if (container.State != "running")
+            {
+                var started = await client.Containers.StartContainerAsync(container.ID, new ContainerStartParameters());
+                if (!started)
+                    throw new Exception("Cannot start the azurite microsoft docker container.");
             }
 
             return container;
