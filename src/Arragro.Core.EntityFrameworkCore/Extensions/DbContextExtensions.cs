ac10fee575f3d9f7bdbf2973bb26ac07c4a3fa7b
Mike Jewell <mike@arragro.com>
2021-02-26 08:48:28 +1300

    Extended DbContextExtensions

--- src/Arragro.Core.EntityFrameworkCore/Extensions/DbContextExtensions.cs
+++ src/Arragro.Core.EntityFrameworkCore/Extensions/DbContextExtensions.cs
@@ -2,9 +2,11 @@
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Data;
 using System.Data.Common;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Arragro.Core.EntityFrameworkCore.Extensions
 {
@@ -33,6 +35,42 @@ namespace Arragro.Core.EntityFrameworkCore.Extensions
             if (context.Database.CurrentTransaction == null)
                 return null;
             return context.Database.CurrentTransaction.GetDbTransaction();
+        }
+
+        public static async Task<TResult> ExecuteDbCommandCommandAsync<TResult>(this DbContext context, Func<Task<TResult>> func)
+        {
+            var conn = context.Database.GetDbConnection();
+            var connClose = false;
+            if (conn.State != ConnectionState.Open)
+            {
+                await conn.OpenAsync();
+                connClose = true;
+            }
+
+            var result = await func();
+
+            if (connClose)
+                conn.Close();
+
+            return result;
+        }
+
+        public static TResult ExecuteDbCommandCommand<TResult>(this DbContext context, Func<TResult> func)
+        {
+            var conn = context.Database.GetDbConnection();
+            var connClose = false;
+            if (conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+                connClose = true;
+            }
+
+            var result = func();
+
+            if (connClose)
+                conn.Close();
+
+            return result;
         }
     }
 }
