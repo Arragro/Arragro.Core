543ff2af5d03d95b6e88ee74163d418a1f125497
Mike Jewell <mike@arragro.com>
2021-05-11 13:22:50 +1200

    Added ITenantResolver

---
+++ src/Arragro.Core.Common/Extentions/TenantIdResolverExtentions.cs
@@ -0,0 +1,34 @@
+﻿using Arragro.Core.Common.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace Arragro.Core.Common.Extentions
+{
+    public class NoTenantIdResolver : ITenantIdResolver
+    {
+        public Guid ResolveTenantId()
+        {
+            return Guid.Empty;
+        }
+
+        public Task<Guid> ResolveTenantIdAsync()
+        {
+            return Task.FromResult(Guid.Empty);
+        }
+    }
+
+    public static class TenantIdResolverExtentions
+    {
+        public static IServiceCollection ConfigureNoTenantIdResolver(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.AddScoped<ITenantIdResolver, NoTenantIdResolver>();
+        }
+
+        public static IServiceCollection ConfigureTenantIdResolver<T>(this IServiceCollection serviceCollection)
+            where T : class, ITenantIdResolver
+        {
+            return serviceCollection.AddScoped<ITenantIdResolver, T>();
+        }
+    }
+}
---
+++ src/Arragro.Core.Common/Interfaces/ITenantIdResolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Arragro.Core.Common.Interfaces
+{
+    public interface ITenantIdResolver
+    {
+        Task<Guid> ResolveTenantIdAsync();
+        Guid ResolveTenantId();
+    }
+}
