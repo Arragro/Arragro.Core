298f22c28f09c3784c4c4ce0607d223cce7a3fbd
Mike Jewell <mike@arragro.com>
2021-06-02 12:02:04 +1200

    Added ScheduledJobStatus

---
+++ src/Arragro.Core.HostedServices/ScheduledJobStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Arragro.Core.HostedServices
+{
+    public class ScheduledJobStatus
+    {
+        public bool IsRunning { get; set; }
+        public DateTimeOffset RunningDate { get; set; }
+
+        public void SetIsRunning(bool isRunning)
+        {
+            if (!IsRunning && isRunning)
+                RunningDate = DateTimeOffset.UtcNow;
+            IsRunning = isRunning;
+        }
+    }
+}
