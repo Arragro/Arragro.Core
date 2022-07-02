7afe2a7c44f34e0d103a5634d2fbce70f336665a
Michael Jewell <mike@arragro.com>
2022-07-02 16:04:53 +1200

    Added Fastly Extentsions

---
+++ src/Arragro.Core.Fastly/FastlyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Arragro.Core.Fastly
+{
+    public static class FastlyExtensions
+    {
+        public static IServiceCollection ConfigureFastlyClient(this IServiceCollection serviceCollection, FastlyApiTokens fastlyApiTokens)
+        {
+            serviceCollection.AddSingleton(fastlyApiTokens);
+            if (fastlyApiTokens.Enabled)
+            {
+                serviceCollection.AddHttpClient<IFastlyClient, FastlyClient>();   
+            }
+            else
+            {
+                serviceCollection.AddHttpClient<IFastlyClient, DummyFastlyClient>();
+            }
+
+            return serviceCollection;
+        }
+    }
+}
