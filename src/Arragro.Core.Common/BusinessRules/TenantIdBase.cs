b643906aab8a9b7fcfce4e4609484a32a2c56ad0
Michael Jewell <mike@arragro.com>
2025-06-26 20:56:24 +1200

    Added TenantIdBase to BusinessRules

---
+++ src/Arragro.Core.Common/BusinessRules/TenantIdBase.cs
@@ -0,0 +1,15 @@
+﻿using Arragro.Core.Common.Interfaces;
+using System;
+
+namespace Arragro.Core.Common.BusinessRules
+{
+    public class TenantIdBase : ITenantId
+    {
+        public Guid TenantId { get; set; }
+    }
+
+    public class AuditableTenantIdBase<TUserIdType> : Auditable<TUserIdType>, IAuditable<TUserIdType>, ITenantId
+    {
+        public Guid TenantId { get; set; }
+    }
+}
