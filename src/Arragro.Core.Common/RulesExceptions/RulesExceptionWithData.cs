a9043e4998e0e14aa31facc5379fbd1335430924
Michael Jewell <mike@arragro.com>
2025-06-27 11:50:53 +1200

    Added more RulesException models

---
+++ src/Arragro.Core.Common/RulesExceptions/RulesExceptionWithData.cs
@@ -0,0 +1,17 @@
+﻿namespace Arragro.Core.Common.RulesExceptions
+{
+    public class RulesExceptionDto<T> : RulesExceptionDto where T : class
+    {
+        public T OtherData { get; set; }
+
+        public RulesExceptionDto() { }
+
+        public RulesExceptionDto(RulesExceptionDto rulesExceptionDto)
+        {
+            this.Data = rulesExceptionDto.Data;
+            this.ErrorMessages = rulesExceptionDto.ErrorMessages;
+            this.Errors = rulesExceptionDto.Errors;
+            this.RulesExceptionListContainers = rulesExceptionDto.RulesExceptionListContainers;
+        }
+    }
+}
---
+++ src/Arragro.Core.Common/RulesExceptions/RulesHttpException.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+
+namespace Arragro.Core.Common.RulesExceptions
+{
+    public class RulesHttpException<T> : Exception where T : class
+    {
+        public int Code { get; set; }
+        public RulesExceptionDto<T> RulesExceptionDto { get; set; }
+
+        // other fields
+        protected RulesHttpException(int code)
+        {
+            Code = code;
+        }
+
+        public RulesHttpException(int code, string message) : base(message)
+        {
+            RulesExceptionDto = null;
+            Code = code;
+        }
+
+        public RulesHttpException(int code, RulesExceptionDto<T> rulesExceptionDto)
+        {
+            Code = code;
+            RulesExceptionDto = rulesExceptionDto;
+        }
+
+        public override string ToString()
+        {
+            var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+            if (RulesExceptionDto == null)
+                return JsonConvert.SerializeObject(new { Code, ErrorMessages = new[] { base.Message }, Errors = new Dictionary<string, object>() }, jsonSerializerSettings);
+            else
+                return JsonConvert.SerializeObject(
+                    new
+                    {
+                        Code,
+                        Message = RulesExceptionDto.ToString(),
+                        RulesExceptionDto.ErrorMessages,
+                        RulesExceptionDto.Errors,
+                        RulesExceptionDto.OtherData,
+                        RulesExceptionDto.Data,
+                    }, jsonSerializerSettings);
+        }
+    }
+}
