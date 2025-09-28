8618913781f57d0d38c19759df4058791aa1f471
Michael Jewell <mike@arragro.com>
2025-09-28 16:53:17 +1300

    Updated cahcen connection string name

--- src/Arragro.Core.Common/Models/FusionCacheConfigurationSettings.cs
+++ src/Arragro.Core.Common/Models/FusionCacheConfigurationSettings.cs
@@ -14,20 +14,20 @@ namespace Arragro.Core.Common.Models
     {
         public FusionCacheType CacheType { get; set; }
         public string BackplaneRedisConnection { get; set; }
-        public string CacheRedisConnection { get; set; }
+        public string DistributedCacheRedisConnection { get; set; }
 
         public void Validate()
         {
             if (CacheType == FusionCacheType.Backplane && string.IsNullOrEmpty(BackplaneRedisConnection))
                 throw new ArgumentException("If using a backplane, you must supply a redis connection string.");
 
-            if (CacheType == FusionCacheType.Distributed && string.IsNullOrEmpty(CacheRedisConnection))
+            if (CacheType == FusionCacheType.Distributed && string.IsNullOrEmpty(DistributedCacheRedisConnection))
                 throw new ArgumentException("If using a cache, you must supply a redis connection string.");
 
             if (CacheType == FusionCacheType.BackplaneAndDistributed && string.IsNullOrEmpty(BackplaneRedisConnection))
                 throw new ArgumentException("If using a backplane, you must supply a redis connection string.");
 
-            if (CacheType == FusionCacheType.BackplaneAndDistributed && string.IsNullOrEmpty(CacheRedisConnection))
+            if (CacheType == FusionCacheType.BackplaneAndDistributed && string.IsNullOrEmpty(DistributedCacheRedisConnection))
                 throw new ArgumentException("If using a cache, you must supply a redis connection string.");
         }
     }
