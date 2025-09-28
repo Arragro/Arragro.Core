00801d86920a7b9c74344dfa3914fbb601fe1437
Michael Jewell <mike@arragro.com>
2025-09-28 16:51:11 +1300

    Updated Cache Settings

---
+++ src/Arragro.Core.Common/Models/FusionCacheConfigurationSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Arragro.Core.Common.Models
+{
+    public enum FusionCacheType
+    {
+        InMemory,
+        Backplane,
+        Distributed,
+        BackplaneAndDistributed
+    }
+
+    public class FusionCacheConfigurationSettings
+    {
+        public FusionCacheType CacheType { get; set; }
+        public string BackplaneRedisConnection { get; set; }
+        public string CacheRedisConnection { get; set; }
+
+        public void Validate()
+        {
+            if (CacheType == FusionCacheType.Backplane && string.IsNullOrEmpty(BackplaneRedisConnection))
+                throw new ArgumentException("If using a backplane, you must supply a redis connection string.");
+
+            if (CacheType == FusionCacheType.Distributed && string.IsNullOrEmpty(CacheRedisConnection))
+                throw new ArgumentException("If using a cache, you must supply a redis connection string.");
+
+            if (CacheType == FusionCacheType.BackplaneAndDistributed && string.IsNullOrEmpty(BackplaneRedisConnection))
+                throw new ArgumentException("If using a backplane, you must supply a redis connection string.");
+
+            if (CacheType == FusionCacheType.BackplaneAndDistributed && string.IsNullOrEmpty(CacheRedisConnection))
+                throw new ArgumentException("If using a cache, you must supply a redis connection string.");
+        }
+    }
+}
